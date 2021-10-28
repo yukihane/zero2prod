24cf8b65d4ee700d09e060272103109e2e7e2c53
yukihane <yukihane.feather@gmail.com>
2021-10-29 08:59:48 +0900

    未使用 use を削除

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use actix_web::{web, App, HttpRequest, HttpResponse, HttpServer, Responder};
+use actix_web::{web, App, HttpResponse, HttpServer, Responder};
 
 async fn health_check() -> impl Responder {
     HttpResponse::Ok()
