5eebf497b5bf58ba0509d94772e11f9900edb6bc
yukihane <yukihane.feather@gmail.com>
2021-10-29 10:35:43 +0900

    asyncは不要だった

--- tests/health_check.rs
+++ tests/health_check.rs
@@ -14,7 +14,7 @@ async fn health_check_works() {
     assert_eq!(Some(0), response.content_length());
 }
 
-async fn spawn_app() {
+fn spawn_app() {
     let server = zero2prod::run().expect("Failed to bind address");
     let _ = tokio::spawn(server);
 }
